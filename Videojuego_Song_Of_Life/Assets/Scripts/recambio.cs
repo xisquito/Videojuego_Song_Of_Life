8c9b572f3cc87d24098c9e2dd11624f02cd34bfc
xisquito <xiscoibanezmelia@gmail.com>
2022-03-11 11:52:38 +0100

    song

---
+++ Videojuego_Song_Of_Life/Assets/Scripts/recambio.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*public class recambio : MonoBehaviour
+{
+
+    private Animator animator;
+    GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        gameManager = GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (animator != 1)
+        {
+            if ()
+            {
+                //animator.SetTrigger(//Nom de s'animació entre comilles)
+            }
+        }
+    }
+}*/
